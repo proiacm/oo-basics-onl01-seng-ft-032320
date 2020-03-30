a862b9f58f6b7bef69e6e8515b4a36cc6fb01b02
CiaraMaria Proia <proiacm@gmail.com>
2020-03-30 21:32:48 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,40 @@
+# learn spec/01_book_spec.rb
 
+class Book 
 
+  def initialize(title)
+    @title = title
+  end
+  
+  def title 
+    @title
+  end
+  
+  def author=(author)
+    @author = author
+  end
+  
+  def author
+    @author
+  end
+  
+  def page_count=(count)
+    @page_count = count
+  end
+  
+  def page_count
+    @page_count
+  end
+  
+  def genre=(theme)
+    @genre = theme
+  end
+  
+  def genre
+    @genre
+  end
+
+def turn_page
+  puts "Flipping the page...wow, you read fast!"
+end
+end
